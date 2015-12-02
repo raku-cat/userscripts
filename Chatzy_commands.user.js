78bc95d71ad49b778ab96ebcc3527a64067cdc02
raku-cat <raku@rakutiki.tv>
2015-12-02 00:59:10 -0600

    update

--- Chatzy_commands.user.js
+++ Chatzy_commands.user.js
@@ -4,20 +4,24 @@
 // @description Adds extra commands(prefixed with !)
 // @include     http://*.chatzy.*/*
 // @version     1
-// @grant       none
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
-var ref = GM_getValue('ref');
-var input = $('#X91').val();
+// dust
 $('#X91').on('keydown', function (e) {
+  var refUrl = GM_getValue('ref');
+  var input = $('#X91').val();
   if (e.keyCode == 13) {
     if (input == '!ref') {
-      $('#X91').val('test');
+      $('#X91').val(refUrl);
     }
-    //   if (input.substring(0, 9) === '!ref add ') {
-    //GM_setValue('ref', input.substring(9));
-    //   console.log('test');
-    //}
-
+    if (input.substr(0, 9) === '!ref add ') {
+      e.preventDefault();
+      GM_setValue('ref', input.substr(9));
+      $('#X91').val('');
+      alert('Ref URL updated, post with !ref');
+      console.log(refUrl)
+    }
   }
 });
