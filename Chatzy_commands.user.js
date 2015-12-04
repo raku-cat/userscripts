a7fb1b7cb34d65f1f20aec8085e8dae80b6bb8a2
raku-cat <raku@rakutiki.tv>
2015-12-03 22:14:58 -0600

    Merge start

--- Chatzy_commands.user.js
+++ Chatzy_commands.user.js
@@ -27,17 +27,5 @@ $('#X91').on('keydown', function (e) {
       $('#X91').val('');
       alert('Command listing, things wrapped in <> should not be typed with the <> surrounding them\n!ref add <name> <url> - Stores the specified url under the specified name.\n!ref <name> - Posts the stored ref with that name.');
     }
-	if (input.substr(0, 7) ===  '!short ') {
-	  if ((input.substr(7) === 'on') || (GM_getValue('short') == 'on')){
-		$('#X91').on('keydown', function (e) {
-  			if (e.keyCode == 13) {
-    			this.value = this.value.replace(/\*([^*]+?)\*/g, '[b]$1[/b]');
-    			this.value = this.value.replace(/\_([^*]+?)\_/g, '[u]$1[/u]');
-    			this.value = this.value.replace(/\^([^*]+?)\^/g, '[i]$1[/i]');
- 			}
-	  	}
-		if ((input.substr(7) === 'off') || (GM_getValue('short') == 'off')){
-			}
-	}
   }
 });
