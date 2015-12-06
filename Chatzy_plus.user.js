92a3e7f7b809a1359ac68b1314e9831e7d28a932
raku-cat <raku@rakutiki.tv>
2015-12-06 02:07:57 -0600

    Added global skin, frame style, and custom fonts

--- Chatzy_plus.user.js
+++ Chatzy_plus.user.js
@@ -3,24 +3,27 @@
 // @namespace   Raku
 // @description Adds extra functionality to chatzy
 // @include     /https?://us1[1-9]|2[1-9]\.chatzy\.(com|org)/*/
-// @version     1.1
+// @version     1.2
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @grant       GM_deleteValue
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 // dust
-
+//Vars for stuff
+var framelink = document.getElementById('X457').href.replace('default', 'frame');
+var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)ChatzySkin\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+var url = document.getElementById('X457').href;
+var urlmatch = url.split(':') [4];
 //First run init
-
 if (GM_getValue('firstrun', '0') == '0') {
-  if (confirm('Welcome to chatzy+!\nThis script adds several new functions to chatzy in the form of input line commands, click OK to learn more about the available commands and features, otherwise you can run !help to bring up the menu later.\nBy continuing to use this script you agree that the author and any contributors are not responsible for anything that may happen.')) {
+  if (confirm('Welcome to chatzy+ version ' + GM_info.script.version + '!\nThis script adds several new functions to chatzy in the form of input line commands, click OK to learn more about the available commands and features, otherwise you can run !help to bring up the menu later.\nBy continuing to use this script you agree that the author and any contributors are not responsible for anything that may happen.')) {
     helpdialog();
     GM_setValue('firstrun', '1');
   } else {
     GM_setValue('firstrun', '1');
   }
 }
-
 //Command hooks
 
 $('#X91').on('keydown', function (e) {
@@ -34,14 +37,18 @@ $('#X91').on('keydown', function (e) {
       $('#X91').val(' ');
       alert('Ref URL added, post with !ref ' + input.split(' ') [2]);
     }
-    if (input == '!help') {      
+    if (input == '!help') {
       $('#X91').val(' ');
       helpdialog();
+    }
+    if (input == '!help font') {
+      alert('To set a custom font(outside of the websafe ones you are limited normally), run !font set <url> and substitute <url> for a url to a google font from here https://www.google.com/fonts for example if you wanted this font https://fonts.googleapis.com/css?family=Oswald you would run !font set https://fonts.googleapis.com/css?family=Oswald\nThe next step is to go in to your account settings and set your font to a custom one(last radial button, should have a text field next to it) and clear whatever is there, and replace it with your font name in single quotes, using the same example as earlier, you would type \'Oswald\' and then click OK and save your prefrences')
+      $('#X91').val(' ');
     }
     if (input.substr(0, 7) == '!short ') {
       if (input.split(' ') [1] == 'on') {
         GM_setValue('short', 'on');
-      } 
+      }
       if (input.split(' ') [1] == 'off') {
         GM_setValue('short', 'off');
       }
@@ -50,9 +57,45 @@ $('#X91').on('keydown', function (e) {
     if (input.substr(0, 8) == '!unaway ') {
       if (input.substr(8) == 'on') {
         GM_setValue('away', 'on');
-      } 
+      }
       if (input.substr(8) == 'off') {
         GM_setValue('away', 'off');
+      }
+      $('#X91').val(' ');
+    }
+    if (input.substr(0, 7) == '!frame ') {
+      if (input.substr(7) == 'on') {
+        GM_setValue('frame', 'on');
+      }
+      if (input.substr(7) == 'off') {
+        GM_setValue('frame', 'off');
+      }
+      $('#X91').val(' ');
+    }
+    if (input.substr(0, 6) == '!skin ') {
+      if (input.substr(6) == 'on') {
+        GM_setValue('skin', 'on');
+      }
+      if (input.substr(6) == 'off') {
+        GM_setValue('skin', 'off');
+      }
+      $('#X91').val(' ');
+    }
+    if (input.substr(0, 6) == '!skin ') {
+      if (input.substr(6) == 'on') {
+        GM_setValue('skin', 'on');
+      }
+      if (input.substr(6) == 'off') {
+        GM_setValue('skin', 'off');
+      }
+      $('#X91').val(' ');
+    }
+    if (input.substr(0, 6) == '!font ') {
+      if (input.split(' ') [1] == 'set') {
+        GM_setValue('font', input.split(' ') [2]);
+      }
+      if (input.split(' ') [1] == 'off') {
+        GM_deleteValue('font');
       }
       $('#X91').val(' ');
     }
@@ -63,14 +106,11 @@ $('#X91').on('keydown', function (e) {
     }
   }
 });
-
 //Function to show help dialog
-
 function helpdialog() {
-  alert('Command listing, things wrapped in <> should not be typed with the <> surrounding them\n!ref add <name> <url> - Stores the specified url under the specified name.\n!ref <name> - Posts the stored ref with that name.\n!short on/off - Turns shorthands on or off, shorthands are as follows:\nWrap text in ^("^the quick brown fox^") for italics, wrap text in * for bold, and wrap text in _ for underline\n!unaway on/off - Turns auto-unsetting of your away on or off');
+  alert('Command listing, things wrapped in <> should not be typed with the <> surrounding them\n!ref add <name> <url> - Stores the specified url under the specified name.\n!ref <name> - Posts the stored ref with that name.\n!short on/off - Turns shorthands on or off, shorthands are as follows:\nWrap text in ^("^the quick brown fox^") for italics, wrap text in * for bold, and wrap text in _ for underline\n!unaway on/off - Turns auto-unsetting of your away on or off\n!skin on/off - Makes your skin you set in account setting apply globaly(all rooms), it will override any rooms with their own skin set as well however\n!frame on/off - Makes chatzy use the stylesheet they use when the chat is embedded in an iframe, might not look pretty\n!font set <url>/off - refer to !help font');
 }
-
-//Auto unaway function
+//Other various functions I'm too lazy to mark
 
 window.setInterval(function () {
   if (GM_getValue('away') == 'on') {
@@ -79,4 +119,17 @@ window.setInterval(function () {
       $('form#X114').submit();
     }
   }
+  if (GM_getValue('frame') == 'on') {
+    if ($('#X457').href != framelink) {
+      document.getElementById('X457').href = document.getElementById('X457').href.replace('default', 'frame');
+    }
+  }
+  if (GM_getValue('skin') == 'on') {
+    if (document.getElementById('X457').href != document.getElementById('X457').href.replace(urlmatch, cookieValue)) {
+      document.getElementById('X457').href = document.getElementById('X457').href.replace(urlmatch, cookieValue);
+    }
+  }
 }, 150);
+if (GM_getValue('font')) {
+  $('head').append('<link href="' + GM_getValue('font') + '" rel="stylesheet" type="text/css">');
+}
